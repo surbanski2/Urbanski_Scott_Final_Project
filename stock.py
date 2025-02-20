c20a52479b49ed5f6d38edd64476271346ec1932
Scott Urbanski <scotturbanski@Scotts-MacBook-Air.local>
2025-02-19 23:43:56 -0600

    Able to now access singular price value.

--- stock.py
+++ stock.py
@@ -4,10 +4,12 @@ class Stock:
 
     def __init__(self, ticker):
         self._ticker = ticker
-        self._price = 0.0
 
     def GetOpeningPrice(self, date):
-        return pdrData.DataReader(name=self._ticker, data_source="stooq", start=date, end=date)
+        data =  pdrData.DataReader(name=self._ticker, data_source="stooq", start=date, end=date)
+        return data.iloc[0]['Open']
+    
+    
 
 
 
