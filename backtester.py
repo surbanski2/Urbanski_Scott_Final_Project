16992708bf0be54af02b646de97d1fc8e3dec0bd
Scott Urbanski <scotturbanski@Scotts-MacBook-Air.local>
2025-02-23 15:13:27 -0600

    Added more documentation.

--- backtester.py
+++ backtester.py
@@ -1,3 +1,17 @@
+"""
+Author: Scott Urbanski
+File : backtester.py
+Date Modified: 2025-02-23
+Description: This GUI application allows the user to enter
+             a stock ticker, a quantity, and a date. The
+             user can then choose to either buy or sell 
+             the stock. After making the desired transactions,
+             the user can then click the Calculate button
+             to see how much the portfolio would have 
+             increased (or decreased) in value at the present
+             day.
+"""
+
 from breezypythongui import EasyFrame
 from portfolio import Portfolio
 from transaction import Transaction
@@ -27,16 +41,36 @@ class Backtester(EasyFrame):
         self.transactionList = self.addTextArea(text="", row=0, column=2, rowspan=3, width=50)
 
     def BuyStock(self): 
+        """
+        Attempts to purchase stock based on the user's inputs
+
+        Arguments:
+        N/A
+
+        Returns:
+        N/A
+        """
+
+        # creates a stock object based on the user's ticker input
         userStock = Stock(self.tickerInput.getText())
+        # tests if the user has entered a valid stock ticker
         if userStock.ValidTicker() == True:
+            # tests if the user has entered a valid quantity
             if self.ValidQuantity() == True:
+                # tests if the user has entered a valid date
                 if self.ValidDate() == True:
+                    # tests if the stock market was open on the date the user selected
                     if userStock.MarketOpen(self.dateInput.getText()) == True:
+                        # tests if the user can afford to purchase the stock he/she wants to purchase
                         if self.myPortfolio.AbleToBuy(float(self.quantityInput.getText()), userStock.GetOpeningPrice(self.dateInput.getText())) == True:
+                            # if the user can afford it, then the stock ticker and its quantity are added to the portfolio
                             self.myPortfolio.AddStock(userStock._ticker, float(self.quantityInput.getText()))
+                            # the appropriate amount of cash is removed from the portfolio
                             self.myPortfolio.DebitCash(float(self.quantityInput.getText()), userStock.GetOpeningPrice(self.dateInput.getText()))
+                            # a transaction is added to the list of transactions
                             self.myTransactions.append(Transaction(True, float(self.quantityInput.getText()), userStock._ticker,userStock.GetOpeningPrice(self.dateInput.getText()), self.dateInput.getText()))
-                            self.transactionList.appendText(self.myTransactions[-1].outputString() + "\n")
+                            # the most recent transaction is outputted to the text area to confirm the purchase was successful
+                            self.transactionList.appendText(self.myTransactions[-1].OutputString() + "\n")
                         else:
                             self.messageBox(title="Error", message="You cannot afford to purchase the entered quantity!")
                     else:
@@ -48,16 +82,27 @@ class Backtester(EasyFrame):
                 self.messageBox(title="Error", message="Quantity of shares must be positive!")
         else:
             self.messageBox(title="Error", message="You have entered an invalid ticker!")
-
-        
+        # all the entry fields are reset to their original state
         self.ResetFields()
 
     def SellStock(self):
         pass
 
     def ValidQuantity(self):
+        """
+        Tests if the user has entered a valid quantity
+
+        Arguments:
+        N/A
+
+        Returns:
+        validQuantity: a Boolean value determining whether or not the user has entered an appropriate quantity
+        """
+        # the user is assumed to have entered a valid quantity
         validQuantity = True
         try:
+            # the user's input is converted to a float and checked to make sure it is greater than zero
+            # if it is less than or equal to zero, or if an error has occurred, validQuantity is set to False
             if float(self.quantityInput.getText()) <= 0:
                 validQuantity = False
         except:
@@ -65,8 +110,20 @@ class Backtester(EasyFrame):
         return validQuantity
     
     def ValidDate(self):
+        """
+        Tests if the user has entered a valid date
+
+        Arguments:
+        N/A
+
+        Returns:
+        validDate: a Boolean value determining whether or not the user has entered an appropriate date
+        """
+        # the user is assumed to have entered a valid date
         validDate = True
         try:
+            # the parser module's parse method attempts to parse the date the user has supplied
+            # if it fails to parse the date, then an error has occurred and validDate os set to False
             parser.parse(self.dateInput.getText())
         except:
             validDate = False
@@ -75,6 +132,16 @@ class Backtester(EasyFrame):
 
     
     def ResetFields(self):
+        """
+        Returns the entry fields to their original state
+
+        Arguments:
+        N/A
+
+        Returns:
+        N/A
+        """
+        
         self.tickerInput.setText("")
         self.quantityInput.setText("0")
         self.dateInput.setText("yyyy-mm-dd")
