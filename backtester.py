35129e799ef17dcfb1832e8a5ced02417332c5d5
Scott Urbanski <scotturbanski@Scotts-MacBook-Air.local>
2025-02-23 17:53:01 -0600

    Finished building date range method and testing.

--- backtester.py
+++ backtester.py
@@ -27,7 +27,7 @@ class Backtester(EasyFrame):
         EasyFrame.__init__(self)
         self.myPortfolio = Portfolio(10000)
         self.myTransactions = []
-        self.currentDate = "2005-01-01"
+        self.currentDate = "1900-01-01"
         self.setTitle("Portfolio Backtester")
         self.setResizable(False)
         self.tickerLabel = self.addLabel(text="Ticker:", row=0, column=0)
@@ -53,9 +53,6 @@ class Backtester(EasyFrame):
         N/A
         """
 
-
-
-        """
         # creates a stock object based on the user's ticker input
         userStock = Stock(self.tickerInput.getText())
         # tests if the user has entered a valid stock ticker
@@ -64,22 +61,26 @@ class Backtester(EasyFrame):
             if self.ValidQuantity() == True:
                 # tests if the user has entered a valid date
                 if self.ValidDate() == True:
-                    # tests if the stock market was open on the date the user selected
-                    if userStock.MarketOpen(self.dateInput.getText()) == True:
-                        # tests if the user can afford to purchase the stock he/she wants to purchase
-                        if self.myPortfolio.AbleToBuy(float(self.quantityInput.getText()), userStock.GetOpeningPrice(self.dateInput.getText())) == True:
-                            # if the user can afford it, then the stock ticker and its quantity are added to the portfolio
-                            self.myPortfolio.AddStock(userStock._ticker, float(self.quantityInput.getText()))
-                            # the appropriate amount of cash is removed from the portfolio
-                            self.myPortfolio.DebitCash(float(self.quantityInput.getText()), userStock.GetOpeningPrice(self.dateInput.getText()))
-                            # a transaction is added to the list of transactions
-                            self.myTransactions.append(Transaction(True, float(self.quantityInput.getText()), userStock._ticker,userStock.GetOpeningPrice(self.dateInput.getText()), self.dateInput.getText()))
-                            # the most recent transaction is outputted to the text area to confirm the purchase was successful
-                            self.transactionList.appendText(self.myTransactions[-1].OutputString() + "\n")
+                    if self.AcceptableDate() == True:
+                        # tests if the stock market was open on the date the user selected
+                        if userStock.MarketOpen(self.dateInput.getText()) == True:
+                            # tests if the user can afford to purchase the stock he/she wants to purchase
+                            if self.myPortfolio.AbleToBuy(float(self.quantityInput.getText()), userStock.GetOpeningPrice(self.dateInput.getText())) == True:
+                                # if the user can afford it, then the stock ticker and its quantity are added to the portfolio
+                                self.myPortfolio.AddStock(userStock._ticker, float(self.quantityInput.getText()))
+                                # the appropriate amount of cash is removed from the portfolio
+                                self.myPortfolio.DebitCash(float(self.quantityInput.getText()), userStock.GetOpeningPrice(self.dateInput.getText()))
+                                # a transaction is added to the list of transactions
+                                self.myTransactions.append(Transaction(True, float(self.quantityInput.getText()), userStock._ticker,userStock.GetOpeningPrice(self.dateInput.getText()), self.dateInput.getText()))
+                                # the most recent transaction is outputted to the text area to confirm the purchase was successful
+                                self.transactionList.appendText(self.myTransactions[-1].OutputString() + "\n")
+                                self.currentDate = self.dateInput.getText()
+                            else:
+                                self.messageBox(title="Error", message="You cannot afford to purchase the entered quantity!")
                         else:
-                            self.messageBox(title="Error", message="You cannot afford to purchase the entered quantity!")
+                            self.messageBox(title="Error", message="The stock market was not open!")  
                     else:
-                        self.messageBox(title="Error", message="The stock market was not open!")
+                        self.messageBox(title="Error", message="You must enter a date between the ranges.")
                 else:
                     self.messageBox(title="Error", message="You have entered an invalid date!")
 
@@ -88,13 +89,9 @@ class Backtester(EasyFrame):
         else:
             self.messageBox(title="Error", message="You have entered an invalid ticker!")
         # all the entry fields are reset to their original state
-
-        """
-        self.AcceptableDate()
         self.ResetFields()
 
-        print(self.myPortfolio._stocks)
-        print(self.myPortfolio._cash)
+        print(self.currentDate)
 
     def SellStock(self):
         """
@@ -115,25 +112,28 @@ class Backtester(EasyFrame):
             if self.ValidQuantity() == True:
                 # tests if the user has entered a valid date
                 if self.ValidDate() == True:
-                    # tests if the stock market was open on the date the user selected
-                    if userStock.MarketOpen(self.dateInput.getText()) == True:
-                        # tests if the user has the stock in the portfolio to sell
-                        if self.myPortfolio.AbleToSell(float(self.quantityInput.getText()), userStock._ticker) == True:
-                            # if the user can sell it, then the stock ticker and its quantity are removed from the portfolio
-                            self.myPortfolio.RemoveStock(userStock._ticker, float(self.quantityInput.getText()))
-                            # the appropriate amount of cash is added from the portfolio
-                            self.myPortfolio.CreditCash(float(self.quantityInput.getText()), userStock.GetClosingPrice(self.dateInput.getText()))
-                            # a transaction is added to the list of transactions
-                            self.myTransactions.append(Transaction(False, float(self.quantityInput.getText()), userStock._ticker,userStock.GetClosingPrice(self.dateInput.getText()), self.dateInput.getText()))
-                            # the most recent transaction is outputted to the text area to confirm the purchase was successful
-                            self.transactionList.appendText(self.myTransactions[-1].OutputString() + "\n")
+                    if self.AcceptableDate() == True:
+                         # tests if the stock market was open on the date the user selected
+                        if userStock.MarketOpen(self.dateInput.getText()) == True:
+                            # tests if the user has the stock in the portfolio to sell
+                            if self.myPortfolio.AbleToSell(float(self.quantityInput.getText()), userStock._ticker) == True:
+                                # if the user can sell it, then the stock ticker and its quantity are removed from the portfolio
+                                self.myPortfolio.RemoveStock(userStock._ticker, float(self.quantityInput.getText()))
+                                 # the appropriate amount of cash is added from the portfolio
+                                self.myPortfolio.CreditCash(float(self.quantityInput.getText()), userStock.GetClosingPrice(self.dateInput.getText()))
+                                # a transaction is added to the list of transactions
+                                self.myTransactions.append(Transaction(False, float(self.quantityInput.getText()), userStock._ticker,userStock.GetClosingPrice(self.dateInput.getText()), self.dateInput.getText()))
+                                # the most recent transaction is outputted to the text area to confirm the purchase was successful
+                                self.transactionList.appendText(self.myTransactions[-1].OutputString() + "\n")
+                                self.currentDate = self.dateInput.getText()
+                            else:
+                                self.messageBox(title="Error", message="You do not have enough shares to sell!")
                         else:
-                            self.messageBox(title="Error", message="You do not have enough shares to sell!")
+                            self.messageBox(title="Error", message="The stock market was not open!")
                     else:
-                        self.messageBox(title="Error", message="The stock market was not open!")
+                        self.messageBox(title="Error", message="The date must be between the previous transaction date and the present day!")
                 else:
                     self.messageBox(title="Error", message="You have entered an invalid date!")
-
             else:
                 self.messageBox(title="Error", message="Quantity of shares must be positive!")
         else:
@@ -184,14 +184,12 @@ class Backtester(EasyFrame):
         return validDate
     
     def AcceptableDate(self):
-        if parser.parse(self.currentDate) < parser.parse(self.dateInput.getText()):
-            print("You have entered a date that is after the current date")
-        else:
-            print("You have entered a date before the current date!")
-        if parser.parse(self.dateInput.getText()) < datetime.datetime.now():
-            print("You have entered an acceptable date.")
-        else:
-            print("You have entered a date that is in the future!")
+        acceptableDate = True
+        if parser.parse(self.currentDate) > parser.parse(self.dateInput.getText()):
+            acceptableDate = False
+        if parser.parse(self.dateInput.getText()) > datetime.datetime.now():
+            acceptableDate = False
+        return acceptableDate
     
     def CaclulatePortfolio(self):
         self.portfolioLabel["text"] = f"Portfolio Value: ${self.myPortfolio.CalculatePortfolioValue() :,.2f}"
